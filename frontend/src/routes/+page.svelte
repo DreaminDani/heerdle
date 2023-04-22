1616df372bd48d29977b6dfded9406d43201ad60
Daniel Sandoval <daniel@desandoval.net>
2023-04-21 19:31:45 -0600

    got skipping to work

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,14 +6,41 @@
 	const artists = track.artists.map((artist) => artist.name);
 	const year = track.album.release_date.split('-')[0];
 
+	/**
+	 * @type {EventTarget | null}
+	 */
+	let player;
+
 	let revealed = false;
 	function reveal() {
 		revealed = true;
 	}
+	let endTime = 1;
+	let skipTime = 1;
+	function skip() {
+		endTime = endTime + skipTime;
+		skipTime = skipTime + 1;
+		if (player && player.paused) {
+			player.currentTime = 0;
+		}
+	}
+
+	/**
+	 * @param {Event} event
+	 */
+	function controlTime(event) {
+		if (!player) {
+			player = event.target;
+		}
+		if (player.currentTime > endTime) {
+			player.pause();
+		}
+	}
 </script>
 
 <h1>Try it</h1>
-<audio controls src={track['preview_url']} />
+<audio on:timeupdate={controlTime} controls src={track['preview_url']} />
+<button on:click={skip}>Skip ({skipTime}s)</button>
 <button on:click={reveal}>Reveal</button>
 {#if revealed}
 	<p>{artists.toString()} - {track.name} ({year})</p>
